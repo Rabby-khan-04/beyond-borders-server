2a714a72f85d8500b9bddae793f655b8310e3eec
Rabby-khan-04 <ajrabbyk72@gmail.com>
2025-01-04 21:42:31 +0600

    add new tourist spot api created

--- src/controllers/touristSpot.controller.js
+++ src/controllers/touristSpot.controller.js
@@ -1,9 +1,12 @@
+import { ObjectId } from "mongodb";
 import { database } from "../db/index.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
 
 const Slider = database.collection("slider");
 const Countries = database.collection("countries");
+const TouristsSpots = database.collection("tourists-spots");
 
 const getAllSlider = asyncHandler(async (req, res) => {
   const cursor = Slider.find();
@@ -25,6 +28,72 @@ const getAllCountries = asyncHandler(async (req, res) => {
     );
 });
 
-const TouristSpotController = { getAllSlider, getAllCountries };
+const getAllTouristsSpots = asyncHandler(async (req, res) => {
+  const cursor = TouristsSpots.find();
+  const touriestsSpots = await cursor.toArray();
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        touriestsSpots,
+        "Touriests Spots Data Fetched Successfully!!!"
+      )
+    );
+});
+
+const getTouristSpot = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const query = { _id: new ObjectId(id) };
+
+  const touristSpot = await TouristsSpots.findOne(query);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        touristSpot,
+        "Touriest Spot Data Fetched Successfully!!!"
+      )
+    );
+});
+
+const addATouristSpot = asyncHandler(async (req, res) => {
+  const spotData = req.body;
+
+  if (!spotData.user_email) {
+    throw new ApiError(409, "User email is required");
+  }
+
+  spotData.average_cost = parseInt(spotData.average_cost);
+  spotData.totalVisitorsPerYear = parseInt(spotData.totalVisitorsPerYear);
+
+  const result = await TouristsSpots.insertOne(spotData);
+
+  const createdSpot = await TouristsSpots.findOne({ _id: result.insertedId });
+
+  if (!createdSpot) {
+    throw new ApiError(
+      500,
+      "Something went wrong while creating tourists spot"
+    );
+  }
+
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(200, createdSpot, "Tourist spot added successfully!!")
+    );
+});
+
+const TouristSpotController = {
+  getAllSlider,
+  getAllCountries,
+  getAllTouristsSpots,
+  getTouristSpot,
+  addATouristSpot,
+};
 
 export default TouristSpotController;
--- src/routes/touristsSpot.routes.js
+++ src/routes/touristsSpot.routes.js
@@ -5,5 +5,8 @@ const router = Router();
 
 router.route("/sliders").get(TouristSpotController.getAllSlider);
 router.route("/countries").get(TouristSpotController.getAllCountries);
+router.route("/spots").get(TouristSpotController.getAllTouristsSpots);
+router.route("/spot/:id").get(TouristSpotController.getTouristSpot);
+router.route("/add-spot").post(TouristSpotController.addATouristSpot);
 
 export default router;
