93cb3db4495fa01e60a4cc601bb7870ef9702086
Rabby-khan-04 <ajrabbyk72@gmail.com>
2025-01-05 20:27:56 +0600

    added vercel config file

--- src/app.js
+++ src/app.js
@@ -6,27 +6,19 @@ const app = express();
 // middlewares
 app.use(
   cors({
-    origin: process.env.CROSS_ORIGIN,
+    origin: process.env.CROSS_ORIGIN || "*",
     credentials: true,
   })
 );
+app.options("*", cors());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
 // router import
 import touristSpotRouter from "./routes/touristsSpot.routes.js";
-import ApiResponse from "./utils/ApiResponse.js";
 
 // Router use
 app.use("/api/v1/tourists-spot", touristSpotRouter);
-
-app.use("/", async (req, res) =>
-  res
-    .status(200)
-    .json(
-      new ApiResponse(200, {}, "Beyond Border server is running on express")
-    )
-);
 
 export default app;
